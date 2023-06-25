c482e7a97f22ee5bf7db3ba92a6d5daf9237d8e6
Emilio Henrique Correa <emiliohc@pbtech.net.br>
2023-06-25 12:17:33 -0300

    Initial Commit

---
+++ Dice-SwiftUI/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  Dice-SwiftUI
+//
+//  Created by Emilio Henrique Corrêa on 25/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(diceFace: leftDiceNumber)
+                    DiceView(diceFace: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }){
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let diceFace: Int
+    
+    var body: some View {
+        Image("dice\(diceFace)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
---
+++ Dice-SwiftUI/Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIApp.swift
+//  Dice-SwiftUI
+//
+//  Created by Emilio Henrique Corrêa on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
